6f7d0c2c96cf8cc761f333f42e27fd3d26a64343
Mahir <mahirmanandhar02@gmail.com>
2024-06-16 22:30:04 +0545

    graph

--- Frontend/Crop/src/App.jsx
+++ Frontend/Crop/src/App.jsx
@@ -1,42 +1,15 @@
-// /* eslint-disable react-hooks/exhaustive-deps */
-// /* eslint-disable no-unused-vars */
-// // eslint-disable-next-line no-unused-vars
-// import React, { useEffect, useState } from "react";
-// import Dashboard from "./Pages/Dashboard.jsx";
-// // import DiseaseDetection from "./Pages/DiseaseDetection.jsx"
-// // import FirstPage from "./Pages/FirstPage.jsx"
-// // import OurTeam from "./Pages/OurTeam.jsx"
-// // import OurServices from "./Pages/OurServices.jsx"
-// // import Navbar from "./Components/Navbar.jsx"
-// import getFormattedWeatherData from "./weatherService";
-
-// export default function Homepage() {
-//   const [weather, setWeather] = useState(null);
-//   const fetchWeather = async () => {
-//     await getFormattedWeatherData({ q: "dhulikhel" }).then((data) => {
-//       setWeather(data);
-//     });
-//   };
-//   fetchWeather();
-//   console.log(weather);
-//   return (
-//     <div>
-//       {/* Yaha Jun MA Aafule Kam Garexa Tyo Wala Component matra rakha aru comment gara below
-//      // <Navbar/>
-//     // <FirstPage/>
-//     // <OurServices/>
-
-//     // <DiseaseDetection/>*/}
-//       <Dashboard title="3 hour forecast" data={weather.threeHourly} />
-//     </div>
-//   );
-// }
-
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import Dashboard from "./Pages/Dashboard.jsx";
 import getFormattedWeatherData from "./weatherService";
+import DiseaseDetection from "./Pages/DiseaseDetection.jsx";
+import FirstPage from "./Pages/FirstPage.jsx";
+import OurTeam from "./Pages/OurTeam.jsx";
+import OurServices from "./Pages/OurServices.jsx";
+import Navbar from "./Components/Navbar.jsx";
+import graph from "./Pages/graph.jsx";
+import "./App.css";
 
 export default function Homepage() {
   const [weather, setWeather] = useState(null);
@@ -54,52 +27,18 @@ export default function Homepage() {
 
   return (
     <div>
-      {/* Yaha Jun MA Aafule Kam Garexa Tyo Wala Component matra rakha aru comment gara below 
-     // <Navbar/>
-    // <FirstPage/>
-    // <OurServices/>
-    
-    // <DiseaseDetection/>*/}
+      <Navbar />
+      <FirstPage />
+      <OurServices />
+      <OurTeam />
       {weather && (
         <Dashboard
           title="3 hour forecast"
           data={weather.formattedForecastWeather.threeHourly}
         />
       )}
+      <DiseaseDetection />
+      <graph />
     </div>
-// eslint-disable-next-line no-unused-vars
-import React from "react";
-
-import Dashboard from "./Pages/Dashboard.jsx"
-import DiseaseDetection from "./Pages/DiseaseDetection.jsx"
-import FirstPage from "./Pages/FirstPage.jsx"
-import OurTeam from "./Pages/OurTeam.jsx"
-import OurServices from "./Pages/OurServices.jsx"
-import Navbar from "./Components/Navbar.jsx"
-import graph from "./Pages/graph.jsx" 
-import './App.css'
-
-
-export default function Homepage()
-{
-  return(
-  <div>
-
-    
-
-   
-   
-  <Navbar/>
-  <FirstPage/>
-  <OurServices/> 
-  <OurTeam/>
-  <Dashboard/>
-  <DiseaseDetection/> 
-  <graph/>
- 
- 
-
-  </div>
-
   );
 }
