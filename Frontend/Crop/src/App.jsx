7ca5fd60c75b3a31d7bb1d473e5140927fccb1b8
Mahir <mahirmanandhar02@gmail.com>
2024-06-16 11:41:37 +0545

    khai k ho

--- Frontend/Crop/src/App.jsx
+++ Frontend/Crop/src/App.jsx
@@ -1,10 +1,11 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
-import Dashboard from "./Pages/Dashboard.jsx"
-import DiseaseDetection from "./Pages/DiseaseDetection.jsx"
-import FirstPage from "./Pages/FirstPage.jsx"
-import OurTeam from "./Pages/OurTeam.jsx"
-import OurServices from "./Pages/OurServices.jsx"
-import Navbar from "./Components/Navbar.jsx"
+// import Dashboard from "./Pages/Dashboard.jsx"
+// import DiseaseDetection from "./Pages/DiseaseDetection.jsx"
+// import FirstPage from "./Pages/FirstPage.jsx"
+// import OurTeam from "./Pages/OurTeam.jsx"
+// import OurServices from "./Pages/OurServices.jsx"
+// import Navbar from "./Components/Navbar.jsx"
 
 
 
@@ -14,11 +15,11 @@ export default function Homepage()
   <>
   {/* Yaha Jun MA Aafule Kam Garexa Tyo Wala Component matra rakha aru comment gara below */}
     
-    <Navbar/>
-    <FirstPage/>
-    <OurServices/>
-    <Dashboard/>
-    <DiseaseDetection/>
+    // <Navbar/>
+    // <FirstPage/>
+    // <OurServices/>
+    // <Dashboard/>
+    // <DiseaseDetection/>
     
   </>
   );
